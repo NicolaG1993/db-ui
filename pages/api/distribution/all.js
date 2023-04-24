0952337264bd539e3865f08506a8bba2b1f1972d
Nicola Gaioni <nicog.designer@gmail.com>
2023-04-25 01:32:46 +0200

    fixed bug  in API

--- pages/api/distribution/all.js
+++ pages/api/distribution/all.js
@@ -1,4 +1,4 @@
-import { getAllDistributions } from "@/utils/db/db";
+import { getAllDistributions, getAllRelations } from "@/utils/db/db";
 
 export default async function handler(req, res) {
     try {
