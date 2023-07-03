844f5003423d94bde71e69ded783f95531a9d3c5
Nicola Gaioni <nicog.designer@gmail.com>
2023-07-03 21:50:37 +0200

    db migration

--- utils/db/db.js
+++ utils/db/db.js
@@ -1,12 +1,12 @@
 import { Pool } from "pg";
 
 const config = {
-    // connectionString: process.env.DATABASE_URL,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PSW,
-    database: process.env.DATABASE_NAME,
+    connectionString: process.env.DATABASE_URL,
+    // host: process.env.DATABASE_HOST,
+    // port: process.env.DATABASE_PORT,
+    // user: process.env.DATABASE_USER,
+    // password: process.env.DATABASE_PSW,
+    // database: process.env.DATABASE_NAME,
 };
 
 let db;
