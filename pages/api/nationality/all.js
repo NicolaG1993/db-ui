7bd537fd56bd2ba0953fa82cc28d5ef3b3555e6c
Nicola Gaioni <nicog.designer@gmail.com>
2023-10-10 20:11:06 +0200

    more imports fixes

--- pages/api/nationality/all.js
+++ pages/api/nationality/all.js
@@ -1,4 +1,4 @@
-import allNationalities from "@/utils/allNationalities";
+import allNationalities from "@/src/application/settings/allNationalities";
 
 export default async function handler(req, res) {
     res.status(200).send(allNationalities);
