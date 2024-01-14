4bf62cc6d934c0a7250e04b87c871b5407a95054
Nicola93 <nicola@DESKTOP-5C6P2KF>
2024-01-14 06:30:42 +0100

    Adding +1/-1 buttons to PageNav

--- src/domains/all/components/Filters/PageNav/PageNav.js
+++ src/domains/all/components/Filters/PageNav/PageNav.js
@@ -1,24 +1,40 @@
+import styles from "@/src/domains/all/components/Filters/PageNav/PageNav.module.css";
 export default function PageNav({ totalCount, goToPage, step, selectedPage }) {
     if (totalCount) {
+        const options = [...Array(Math.ceil(totalCount / step))].map(
+            (value, i) => (
+                <option key={i} value={i + 1}>
+                    {i + 1}
+                </option>
+            )
+        );
+        const totalPages = options.length;
+
+        console.log({ totalCount, totalPages, goToPage, step, selectedPage });
         return (
-            <div>
-                <span>Page: </span>
+            <div className={styles.wrap}>
+                <button
+                    onClick={
+                        (e) => goToPage(selectedPage - 1, "page") // passo page perché potrebbe servirmi x cookies in futuro
+                    }
+                    disabled={selectedPage <= 1}
+                >
+                    -1
+                </button>
                 <select
                     name="page"
                     id="Page"
-                    onChange={
-                        (e) => goToPage(Number(e.target.value), "page") // passo page perché potrebbe servirmi x cookies in futuro
-                    }
+                    onChange={(e) => goToPage(Number(e.target.value), "page")}
                     value={selectedPage}
                 >
-                    {[...Array(Math.ceil(totalCount / step))].map(
-                        (value, i) => (
-                            <option key={i} value={i + 1}>
-                                {i + 1}
-                            </option>
-                        )
-                    )}
+                    {options}
                 </select>
+                <button
+                    onClick={(e) => goToPage(selectedPage + 1, "page")}
+                    disabled={selectedPage === totalPages}
+                >
+                    +1
+                </button>
             </div>
         );
     }
